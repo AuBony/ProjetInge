1e07c270b732ed810850ecbb5dd4aa52e88dfa71
Anne-Victoire de Croutte <anne-victoire.decroutte@agrocampus-ouest.fr>
2020-12-02 15:01:03 +0100

    Update script_decouv_AV.R

--- script_decouv_AV.R
+++ script_decouv_AV.R
@@ -3,12 +3,11 @@ load("~/2020-2021/PROJET-INGE/data_agrocampus.RData")
 
 library(plotly)
 library(tidyverse)
+library(ggplot2)
 
-# write.table(model_data[c('1','10','32','34','39','40'),], "toy_data.csv", row.names=TRUE, sep=";",dec=",")
-
-toy_data <- model_data[c('1','10','32','34','39','40'),]
 son <- model_data[, 7:1102506]
 chat <- model_data[,1:6]
+toy_data <- model_data[c('1','10','32','34','39','40'),]
 
 toy_chat <- chat[1:10,]
 f <- 25/1102500
@@ -53,7 +52,6 @@ plot(temps,son['40',temps], type = 'l', xlab = "temps", ylab = "son",
 amplitude <- as.integer(son['32',temps])
 time <- temps[-length(temps)]*f
 dta <- data.frame(time = time, amplitude = amplitude)
-plot_ly(x = ~time, y = ~amplitude, mode = 'lines')
 
 p1 <- dta %>% ggplot() +
   aes(x = time, y = amplitude) +
@@ -71,11 +69,85 @@ ggplotly(p1)
 
 # fonction qui prend en entrée la ligne qu'on veut tracer et le pas de temps
 
-draw_plot <- function(line,pas,dep,fin){
+draw_plot <- function(line, pas, dep = 0, fin = 25){
   dt <- 25/1102500
   dep <- dep
   fin <- fin/dt
-  pas <- 500
+  pas <- pas
+  temps <- seq(from = dep, to = fin, by = pas)
+  indice <- (temps + 1)[-length(indice)]
+  dta <- data.frame(time = temps[-length(temps)]*dt, 
+                    amplitude = as.integer(son[line,temps]))
+  
+  # graphe
+  p1 <- dta %>% ggplot() +
+    aes(x = time, y = amplitude) +
+    ylim(-25000,25000) +
+    geom_line(size = 0.2) +
+    theme_minimal()
+  
+  p1 <- p1 + annotate("text", x = 22, y = max(dta$amplitude), 
+                      label = paste('nb_bk: ',
+                                    chat[line,'nb_bk'],'\n',
+                                    'nb_bit: ',
+                                    chat[line,'nb_bit']),
+                      colour = 'orange', size = 4) +
+    ggtitle(paste("Line ",line))
+  ggplotly(p1)
+}
+
+draw_plot('32',200,0,25)
+
+
+# indiv 70 : mâche bcp (4,34)
+# indiv 105 : mâche peu (0,1)
+
+line <- c('32','34')
+dt <- 25/1102500
+dep <- 0
+fin <- 25/dt
+pas <- 500
+temps <- seq(from = dep, to = fin, by = pas)
+indice <- (temps + 1)
+indice <- indice[-length(indice)]
+tps <- rep(indice,length(line))
+amp <- c()
+lines <- c()
+for (i in 1:length(line)){ 
+  amp <- c(amp, as.integer(son[line,indice][i,]))
+  lines <- c(lines,rep(line[i],length(indice)))
+}
+
+dta <- data.frame(time = tps*dt, 
+                  amplitude = amp,
+                  line = as.factor(lines))
+
+# graphe
+p1 <- ggplot(dta, aes(x = time, y = amplitude)) +
+  ylim(-25000,25000) +
+  geom_line(size = 0.2) +
+  theme_minimal()
+
+p1 + facet_grid(.~lines)
+
+p1 + annotate("text", x = 22, y = max(dta$amplitude), 
+                    label = paste('nb_bk: ',
+                                  chat[line,'nb_bk'],'\n',
+                                  'nb_bit: ',
+                                  chat[line,'nb_bit']),
+                    colour = 'orange', size = 4) +
+  ggtitle(paste("Line ",line))
+ggplotly(p1) 
+
+
+############################################
+############################################
+
+draw_plot <- function(line, pas, dep = 0, fin = 25){
+  dt <- 25/1102500
+  dep <- dep
+  fin <- fin/dt
+  pas <- pas
   temps <- seq(from = dep, to = fin, by = pas)
   indice <- temps + 1
   dta <- data.frame(time = temps[-length(temps)]*dt, 
@@ -85,15 +157,33 @@ draw_plot <- function(line,pas,dep,fin){
   p1 <- dta %>% ggplot() +
     aes(x = time, y = amplitude) +
     geom_line(size = 0.2) +
+    ylim(-25000,25000) +
     theme_minimal()
   
-  p1 <- p1 + annotate("text", x = 23, y = max(dta$amplitude), 
+  p1 <- p1 + annotate("text", x = 22, y = 20000, 
                       label = paste('nb_bk: ',
                                     chat[line,'nb_bk'],'\n',
                                     'nb_bit: ',
                                     chat[line,'nb_bit']),
-                      colour = 'orange', size = 5)
+                      colour = 'orange', size = 4) +
+    annotate("text", x = 3, y = 24000, 
+             label = paste("Line ",line),
+             colour = 'black', size = 5)
   ggplotly(p1)
 }
 
-draw_plot('39',200,0,25)
+manygraphs <- function(rows, vec_lines, pas, dep, fin){
+  p1 <- draw_plot(vec_lines[1], pas, dep, fin)
+  p2 <- draw_plot(vec_lines[2], pas, dep, fin)
+  p3 <- draw_plot(vec_lines[3], pas, dep, fin)
+  p4 <- draw_plot(vec_lines[4], pas, dep, fin)
+  subplot(p1, p2, p3, p4, nrows = rows)
+}
+
+vec_lines <- c('1','34','70','105')
+pas <- 200
+dep <- 0
+fin <- 25
+rows <- 2
+manygraphs(rows, vec_lines, pas, dep, fin)
+
