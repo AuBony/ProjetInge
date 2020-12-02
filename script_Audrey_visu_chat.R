52d5419ba892ad80ddf34fee49c847985cc0b383
AuBony <audrey.bony@yahoo.fr>
2020-12-02 13:28:27 +0100

    Script visu Chat
    Scripte pour les données Chats
    Graph :
     - Histo croquage
     - Histo machage
     - Plotly pour visualiser les chats en fonction du nombre de croquage et de machage

--- script_Audrey_visu_chat.R
+++ script_Audrey_visu_chat.R
@@ -4,3 +4,20 @@
 # 2/12/2020
 
 
+############################
+# Visualisation exp?rience #
+############################
+#Distribution du nombre de machage
+hist(chat$nb_bit)
+
+#Distribution du nombre de croquage
+hist(chat$nb_bk)
+
+#Nombre de croquage et nombre de mâchage 
+library(plotly)
+plot_ly(chat, x = ~nb_bit, y = ~nb_bk, text = rownames(chat),
+        type = 'scatter',
+        mode = 'markers',
+        marker = list(size = 10,color = 'rgba(255, 182, 193, .9)',
+                      line = list(color = 'rgba(152, 0, 0, .8)',
+                                  width = 2)))
