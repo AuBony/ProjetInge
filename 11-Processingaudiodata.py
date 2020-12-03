754ac16db304b234310925f09b5762975f4cdaa3
AuBony <audrey.bony@yahoo.fr>
2020-12-03 16:47:40 +0100

    Update 11-Processingaudiodata.py

--- 11-Processingaudiodata.py
+++ 11-Processingaudiodata.py
@@ -34,4 +34,26 @@ plt.show()
 # stft -> spectogram
 n_fft = 2048 #number sample per fft
 hop_length = 512
-stft = librosa.core.stft(signal)
+
+stft = librosa.core.stft(signal, hop_length = hop_length, n_fft = n_fft)
+spectrogram = np.abs(stft)
+
+log_spectrogram = librosa.amplitude_to_db(spectrogram)
+
+librosa.display.specshow(log_spectrogram, sr = sr, hop_length = hop_length)
+plt.xlabel("Time")
+plt.ylabel("Frequency")
+plt.colorbar()
+plt.show()
+
+#MFCCs
+MFCCs = librosa.feature.mfcc(signal, n_fft = n_fft, hop_length =  hop_length, n_mfcc = 13)
+
+librosa.display.specshow(MFCCs, sr = sr, hop_length = hop_length)
+plt.xlabel("Time")
+plt.ylabel("MFCC")
+plt.colorbar()
+plt.show()
+
+
+
