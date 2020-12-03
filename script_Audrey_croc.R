a74e52a9620324942f20c3c96cf3d54efd29d4cb
AuBony <audrey.bony@yahoo.fr>
2020-12-03 15:38:33 +0100

    Update script_Audrey_croc.R

--- script_Audrey_croc.R
+++ script_Audrey_croc.R
@@ -5,10 +5,11 @@
 
 #Import des données
 library(tuneR)
+library(seewave)
 notchi2_wav <- readWave("data/Notchi 2.wav")
 
 #Ecoute du son
-listen(notchi2_wav)
+play(notchi2_wav)
   #Notchi croque autour de la seconde 4
 
 #Sélection du son de croquage
@@ -19,15 +20,18 @@ listen(croc_wav)
 #On enregistre le son de croc sous la forme d'un data.frame
 croc <- as.integer(cutw(notchi2_wav, from = 4, to = 4.8, f = notchi2_wav@samp.rate))
 
-#Enveloppe du son
-analysis_croc <- acoustat(croc_wav, fraction = c(50) )
-analysis_croc$freq.P1
-analysis_croc$freq.M
-analysis_croc$freq.P2
-analysis_croc$freq.IPR
-
-par(mfrow = c(1,1))
-env(croc_wav)
-fpeaks(meanspec(croc_wav, wl = 1000), nmax = 1)
-spec(croc_wav)
+#Spectrogramme
 spectro(croc_wav)
+
+#MFCCs
+size <- 0.01
+mfcc_croc <- tuneR::melfcc(croc_wav, wintime = size, hoptime = size)
+class(mfcc_croc)
+
+heatmap(t(mfcc_croc), Colv = NA, Rowv = NA, scale = "column",
+        main = "MFCCs Croc",
+        labCol = seq(0, 0.8, size), 
+        labRow = paste("Coef ", seq(1,12,1)))
+
+
+
