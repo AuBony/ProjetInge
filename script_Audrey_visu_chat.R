f7834d4c5efd3642dc669068a09361ff9edc5f0c
AuBony <audrey.bony@yahoo.fr>
2020-12-04 17:28:50 +0100

    Update script_Audrey_visu_chat.R

--- script_Audrey_visu_chat.R
+++ script_Audrey_visu_chat.R
@@ -3,19 +3,20 @@
 # Audrey Bony
 # 2/12/2020
 
-
+par(mfrow = c(1,1))
 ############################
 # Visualisation exp?rience #
 ############################
+
 #Distribution du nombre de machage
-hist(chat$nb_bit)
+hist(chat2$nb_bk, xlab = "", main = "Histogramme des croquages")
 
 #Distribution du nombre de croquage
-hist(chat$nb_bk)
+hist(chat2$nb_bit, xlab = "", main = "Histogramme des mâchages")
 
 #Nombre de croquage et nombre de mâchage 
 library(plotly)
-plot_ly(chat, x = ~nb_bit, y = ~nb_bk, text = rownames(chat),
+plot_ly(chat2, x = ~nb_bit, y = ~nb_bk, text = rownames(chat2),
         type = 'scatter',
         mode = 'markers',
         marker = list(size = 10,color = 'rgba(255, 182, 193, .9)',
