9777de372b3a5aebe3b14169cc74ae1a5356fda3
AuBony <audrey.bony@yahoo.fr>
2020-12-04 17:29:56 +0100

    Update Audrey-init.R
    Chat2 en factor donc conversion en int

--- Audrey-init.R
+++ Audrey-init.R
@@ -4,7 +4,10 @@
 # 2/12/2020
 
 load(file = "data/Mars.RData")
-load(file = "data/noise_data_agrocampus.RData")
+#load(file = "data/noise_data_agrocampus.RData")
+
+chat2$nb_bk <- as.integer(as.character(chat2$nb_bk))
+chat2$nb_bit <- as.integer(as.character(chat2$nb_bit))
 
 dt <- 25/1102500
 f <- 1/dt
