d7d6dda6f04636ed6fe6224d8ca28f2eb4f0f9cc
AuBony <audrey.bony@yahoo.fr>
2020-12-14 09:51:35 +0100

    Update Audrey_ICA.R

--- Audrey_ICA.R
+++ Audrey_ICA.R
@@ -4,7 +4,7 @@
 # 10/12/2020
 
 load("../data/Mars.RData")
-test <- son2[1:20, 1:1000]
+test <- son2[1:20, 1:1000000]
 
 # JADE ----
 library(JADE)
@@ -42,3 +42,32 @@ plot(1:20, res$S[,2])
 
 res$W
 
+# retest
+son <- as.data.frame(t(as.matrix(test)))
+son_scale <- scale(son, center = FALSE, scale = apply(son, 2, sd))
+St <- ts(son_scale, start = 0, frequency = 44100)
+ 
+jade <- JADE(son_scale)
+sobi <- SOBI(St)
+nsstdjd <- NSS.TD.JD(St)
+
+dim(coef(jade))
+jade$A
+
+Z <- bss.components(jade)
+library(BBmisc)
+library(tuneR)
+NSSTDJDwave1 <- normalize(Wave(left = as.numeric(Z[, 1]), samp.rate = 44100, bit = 16), unit = "16")
+play(NSSTDJDwave1) #voix
+NSSTDJDwave2 <- normalize(Wave(left = as.numeric(Z[, 2]), samp.rate = 44100, bit = 16), unit = "16")
+play(NSSTDJDwave2) #frottement
+NSSTDJDwave3 <- normalize(Wave(left = as.numeric(Z[, 3]), samp.rate = 44100, bit = 16), unit = "16")
+play(NSSTDJDwave3) 
+NSSTDJDwave4 <- normalize(Wave(left = as.numeric(Z[, 4]), samp.rate = 44100, bit = 16), unit = "16")
+play(NSSTDJDwave4) 
+NSSTDJDwave5 <- normalize(Wave(left = as.numeric(Z[, 5]), samp.rate = 44100, bit = 16), unit = "16")
+play(NSSTDJDwave5) 
+
+play(readWave("../data/audio_brute/chat_4.wav"))
+
+plot.ts(Z[,1:5], nc = 1)
