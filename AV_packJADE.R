9813dacb9ff64bf55b855b32cbb46889f8d3f5c9
Anne-Victoire de Croutte <anne-victoire.decroutte@agrocampus-ouest.fr>
2020-12-14 17:47:06 +0100

    Update AV_packJADE.R

--- AV_packJADE.R
+++ AV_packJADE.R
@@ -24,6 +24,17 @@ plot(S[,c(1:7)], plot.type = 'multiple', main = 'Sources 1 à 7')
 plot(S[,c(8:14)], plot.type = 'multiple', main = 'Sources 8 à 14')
 plot(S[,c(15:21)], plot.type = 'multiple', main = 'Sources 15 à 21')
 
+Z <- bss.components(jade)
+Zwave1 <- tuneR:::normalize(Wave(left = as.numeric(Z[, 1]), samp.rate = 220, bit = 16), unit = '16')
+play(Zwave1)
+Zwave13 <- tuneR:::normalize(Wave(left = as.numeric(Z[, 13]), samp.rate = 220, bit = 16), unit = '16')
+play(Zwave13)
+Zwave3 <- normalize(Wave(left = as.numeric(Z[, 3]), samp.rate = 44100, bit = 16), unit = "16")
+play(NSSTDJDwave3) 
+NSSTDJDwave4 <- normalize(Wave(left = as.numeric(Z[, 4]), samp.rate = 44100, bit = 16), unit = "16")
+play(NSSTDJDwave4) 
+NSSTDJDwave5 <- normalize(Wave(left = as.numeric(Z[, 5]), samp.rate = 44100, bit = 16), unit = "16")
+play(NSSTDJDwave5) 
 
 ####
 # SOBI et ?? 
