06345e54caf82507374d1724695be309e463b7b1
Anne-Victoire de Croutte <anne-victoire.decroutte@agrocampus-ouest.fr>
2021-01-19 23:42:28 +0100

    Update AV_detecpic.R

--- AV_detecpic.R
+++ AV_detecpic.R
@@ -1,4 +1,5 @@
-load("~/2020-2021/PROJET-INGE/complete_clean_data.RData")
+load("C:/Users/HP/Documents/GitHub/ProjetInge/complete_clean_data.RData")
+
 
 test <- dta[,c(seq(1,882000, by = 200), 882001, 882002)]
 n <- ncol(test)
@@ -8,3 +9,6 @@ View(test[1:10,c(1:5,4411,4412)])
 
 testcr <- as.data.frame(t(apply(t(test),2,scale)))
 
+l <- 2
+plot(1:(n-2), testcr[l,-c(n-1, n)], type = 'l')
+print(testcr[l,c(n-1, n)])
