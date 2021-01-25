6f851befa3d106582dedf7e8fbc5dba5e4734b54
Anne-Victoire de Croutte <anne-victoire.decroutte@agrocampus-ouest.fr>
2021-01-25 13:44:24 +0100

    Update AV_detecpic.R

--- AV_detecpic.R
+++ AV_detecpic.R
@@ -12,10 +12,11 @@ require(purrr)  # for map(), reduce()
 library(stringr) # for str_replace()
 library(ggplot2)
 library(plotly)
+library(vioplot)
 
 path <- '~/GitHub/ProjetInge/'
 
-# IMPORTATION DES NB_BK
+#---- BREAKS IMPORTATION ----
 
 data_path <- paste0(path,'labels/')
 files <- dir(data_path, pattern = "*.txt")
@@ -54,127 +55,161 @@ df_wav$filename <- str_replace(df_txt$filename, ".txt", ".wav")
 df_wavc <- df_wav %>% filter(annotation == 'croc')
 df_wavc
 
-eff <- df_wavc %>% group_by(filename) %>% summarise(count = n())
-nb_crocs_tot <- sum(eff[[2]])
+eff <- df_wavc %>% group_by(filename) %>% summarise(nb_bk = n())
+nb_bk_tot <- sum(eff$nb_bk)
 
-# COMPTAGE
+#---- FUNCTIONS ----
 
-fich <- list.files(paste0(path,'cleanwav'))
-wavlist <- list(normalize(readWave(paste0(path,'cleanwav/',fich[1])), center = TRUE))
-for (k in 2:length(fich)){
-  wavlist[[k]] <- normalize(readWave(paste0(path,'cleanwav/',fich[k])),center = TRUE)
+imp_norm <- function(file_name, path){
+  # INPUTS : 
+  # file_name : character, name of the wave file to import and normalize
+  # path : character, path to the file
+  # OUTPUT : 
+  # wave file imported and normalized
+  return(normalize(readWave(paste0(path,'cleanwav/',file_name)), center = TRUE))
 }
 
-count <- rep(0,length(wavlist))
-n <- length(wavlist)
-amp_lim <- 0.3
-diff_lim <- 4000
-for (i in 1:n){
+count_peaks <- function(wav, amp_lim, diff_lim){
+  # INPUTS:
+  # wav: Wave, an element of wavlist (a recording as wave)
+  # amp_lim : numeric, amplitude limit of detection
+  # diff_lim : numeric, minimal distance between 2 peaks
+  # OUTPUT:
+  # c : numeric, count of peaks in the wav
   c <- 1
-  vec <- wavlist[[i]]@left
-  vecsel <- which(vec > amp_lim | vec < -amp_lim)
+  vec <- wav@left
+  vecsel <- which(abs(vec) > amp_lim)
   if (length(vecsel) == 0){
     c <- 0
   } else if (length(vecsel) > 1) {
     for (k in 2:length(vecsel)){
-      if (vecsel[k]-vecsel[k-1] > diff_lim){
-        c <- c+1
+      if (abs(vecsel[k]-vecsel[k-1]) > diff_lim){
+        c <- c + 1
       }
     }}
-  count[i] <- c
+  return(c)
 }
-print(count)
 
-## COMPARAISON PREDICTION / COMPTAGE
 
-df <- data.frame(actual = eff[[2]], prediction = count)
-df <- df %>% add_count(prediction)
 
-ggplot(df, aes(x = actual, y = prediction)) +
+#---- COUNTING ----
+
+fich <- data.frame(list.files(paste0(path,'cleanwav')))
+wavlist <- apply(fich, 1, FUN = imp_norm, path = path)
+amp_lim = 0.4
+diff_lim = 4000
+count <- unlist(lapply(X = wavlist, FUN = count_peaks, 
+                       amp_lim = amp_lim, diff_lim = diff_lim))
+
+#---- PREDICTION vs COUNTING ----
+
+df1 <- data.frame(actual = eff$nb_bk, prediction = count)
+df1 <- df1 %>% add_count(prediction)
+
+title1 <- paste0('Number of breaks, predicted vs actual \n for amp_lim = ', amp_lim,
+                'and diff_lim = ', diff_lim)
+ggplot(df1, aes(x = actual, y = prediction)) +
   geom_segment(aes(x = 0, y = 0, xend = 6, yend = 6)) +
   geom_point(aes(col = n)) +
   ylim(0,6) +
   xlim(0,6) +
   theme(plot.title = element_text()) +
-  labs(title = 'Number of breaks predicted vs actual') +
+  labs(title = title1) +
   theme_light()
 
-# variation de la limite d'amplitude
+#---- AMPLITUDE LIMIT OPTIMIZATION ----
 
-mse <- rep(0, 10)
+mse2 <- rep(0, 10)
+diff_lim <- 4400
 for (j in 1:10){
-  count <- rep(0,length(wavlist))
-  n <- length(wavlist)
-  amp_lim <- j*0.1
-  diff_lim <- 4400
-  for (i in 1:n){
-    c <- 1
-    vec <- wavlist[[i]]@left
-    vecsel <- which(vec > amp_lim | vec < -amp_lim)
-    if (length(vecsel) == 0){
-      c <- 0
-    } else if (length(vecsel) > 1) {
-      for (k in 2:length(vecsel)){
-        if (vecsel[k]-vecsel[k-1] > diff_lim){
-          c <- c+1
-        }
-      }}
-    count[i] <- c
-  }
-  mse[j] <- sum((count - eff[[2]])^2) / length(count)
+  count2 <- unlist(lapply(X = wavlist, FUN = count_peaks, 
+                         amp_lim = j*0.1, diff_lim = diff_lim))
+  mse2[j] <- sum((count2 - eff$nb_bk)^2) / length(count2)
   print(j)
 }
 
-df <- data.frame(amp_lim = seq(0.1, 1, by = 0.1), mse = mse) 
-p <- ggplot(df, aes(x = amp_lim, y = mse)) +
+title2 <- paste0('MSE with different amplitude limits, \n with diff_lim = ', 
+                 diff_lim)
+df2 <- data.frame(amp_lim = seq(0.1, 1, by = 0.1), mse = mse2) 
+p2 <- ggplot(df2, aes(x = amp_lim, y = mse)) +
   geom_line() +
   theme_light() +
   theme(plot.title = element_text()) +
-  labs(title = 'MSE with different amplitude limits for selection')
-ggplotly(p)
+  labs(title = title2)
+ggplotly(p2)
 
-# variation de la limite de separation des pics -> devrait pas faire avec mse, 
+amp_lim_opt <- df2$amp_lim[which.min(mse2)]
+
+#---- MINIMAL DISTANCE BETWEEN 2 PEAKS ----
+# variation de la limite de separation des pics 
+# -> devrait pas faire avec mse, 
 # juste moyenne sur nos donnees
 
-mse <- rep(0, 87)
+mse3 <- rep(0, 87)
 a <- 0
+amp_lim <- amp_lim_opt
 for (j in seq(1000, 44000, by = 500)){
   a <- a + 1
-  count <- rep(0,length(wavlist))
-  n <- length(wavlist)
-  amp_lim <- 0.4
-  diff_lim <- j
-  for (i in 1:n){
-    c <- 1
-    vec <- wavlist[[i]]@left
-    vecsel <- which(vec > amp_lim | vec < -amp_lim)
-    if (length(vecsel) == 0){
-      c <- 0
-    } else if (length(vecsel) > 1) {
-      for (k in 2:length(vecsel)){
-        if (vecsel[k]-vecsel[k-1] > diff_lim){
-          c <- c+1
-        }
-      }}
-    count[i] <- c
-  }
-  mse[a] <- sum((count - eff[[2]])^2) / length(count)
-  print(j)
+  count3 <- unlist(lapply(X = wavlist, FUN = count_peaks, 
+                         amp_lim = amp_lim, diff_lim = j))
+  mse3[a] <- sum((count3 - eff$nb_bk)^2) / length(count3)
+  print(a)
 }
 
-df <- data.frame(diff_lim = seq(1000, 44000, by = 500), mse = mse) 
-p <- ggplot(df, aes(x = diff_lim, y = mse)) +
+df3 <- data.frame(diff_lim = seq(1000, 44000, by = 500), mse = mse3) 
+title3 <- paste0('MSE with different minimal distances between peaks, \n with amp_lim = ',
+                 amp_lim)
+p3 <- ggplot(df3, aes(x = diff_lim, y = mse3)) +
   geom_line() +
   theme_light() +
   theme(plot.title = element_text()) +
-  labs(title = 'MSE with different distance between peaks limits for selection')
-ggplotly(p)
+  labs(title = title3)
+ggplotly(p3)
 
-df$diff_lim[which.min(mse)] # 18500
+df3$diff_lim[which.min(mse3)] # 18500
 
 # ecart minimal entre 2 crocs? on va prendre 0.1 sec pour l'instant, apres on pourra
 # regarder la vraie valeur
 # 0.1*44100 = 4410 donc on va prendre un ecart minimal de 4400 unites entre 2 pics
+
+
+bk_length <- data.frame(bk_length = df_wavc$end - df_wavc$start)
+
+sep_length <- c()
+for (k in 2:nrow(df_wavc)){
+  if (df_wavc$filename[k-1] == df_wavc$filename[k]){
+    print(paste(df_wavc$filename[k-1], df_wavc$filename[k], sep = ','))
+    sep_length <- c(sep_length, df_wavc$start[k] - df_wavc$end[k-1])
+  }
+}
+sep_length <- data.frame(sep_length)
+
+ggplot(bk_length, aes(x = bk_length)) +
+  geom_boxplot(aes(y = -0.5, fill = "#69b3a2"), alpha = 0.6, show.legend = FALSE,
+               width = 0.3) +
+  geom_density(aes(x = bk_length), fill = "slateblue", 
+               inherit.aes = FALSE, alpha = 0.6) +
+  ylab('density') +
+  xlab('bk_length (s)') +
+  theme(plot.title = element_text()) +
+  labs(title = 'Breaks length') +
+  scale_x_continuous(breaks = seq(0, 0.8, by = 0.1)) +
+  theme_minimal()
+
+ggplot(sep_length, aes(x = sep_length)) +
+  geom_boxplot(aes(y = -0.1, fill = "#69b3a2"), alpha = 0.6, show.legend = FALSE,
+               width = 0.05) +
+  geom_density(aes(x = sep_length), fill = "slateblue", 
+               inherit.aes = FALSE, alpha = 0.6) +
+  ylab('density') +
+  xlab('sep_length (s)') +
+  theme(plot.title = element_text()) +
+  labs(title = 'Distance between 2 peaks') +
+  scale_x_continuous(breaks = seq(0, 6, by = 0.5)) +
+  theme_minimal()
+
+
+#---- COMMENTS ----
 
 # marche pas si pas de croc car dilate echelle, mais peut-etre a utiliser apres 
 # le tri croc/pas croc pour compter sur le signal entier
