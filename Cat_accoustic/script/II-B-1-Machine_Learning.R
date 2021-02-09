501395701c25bfabdbc90387eb787949ad59b874
AuBony <audrey.bony@yahoo.fr>
2021-02-09 17:03:06 +0100

    Maj IIB1

--- Cat_accoustic/script/II-B-1-Machine_Learning.R
+++ Cat_accoustic/script/II-B-1-Machine_Learning.R
@@ -92,20 +92,20 @@ IIB1_df_feature <- tibble(id = numeric(),
 )
 
 # Run through df_wav line by line
-for (i in 1:nrow(df_wav)){
+for (i in 1:nrow(IIB1_df_wav)){
   # wav_file contains an event (croc or mach)
-  wav_file <- readWave(paste0(wav_path, df_wav[i,2]),
-                       from = df_wav[i,3],
-                       to = df_wav[i,4],
+  wav_file <- readWave(paste0(wav_path, IIB1_df_wav[i,2]),
+                       from = IIB1_df_wav[i,3],
+                       to = IIB1_df_wav[i,4],
                        units = "seconds") 
   
   # Obtain a list of statistical properties of a frequency spectrum
   sp <- specprop(spec(wav_file@left, f = wav_file@samp.rate, plot = FALSE, scaled = TRUE, norm = FALSE))
   
   # Add Features into IIB1_df_feature
-  IIB1_df_feature <- IIB1_df_feature %>% add_row(id = df_wav$id[i],
-                                       filename = df_wav$filename[i],
-                                       annotation = df_wav$annotation[i],
+  IIB1_df_feature <- IIB1_df_feature %>% add_row(id = IIB1_df_wav$id[i],
+                                       filename = IIB1_df_wav$filename[i],
+                                       annotation = IIB1_df_wav$annotation[i],
                                        
                                        th = th(env(wav_file, plot = FALSE)),
                                        maxdfreq = max(dfreq(wav_file, plot = FALSE)[,2]),
@@ -126,4 +126,79 @@ for (i in 1:nrow(df_wav)){
                                        ssh = sp$sh,
                                        sprec = sp$prec
   ) 
+  remove(sp, wav_file)
 }
+
+## MODEL ----
+## Goal : 
+## Input : IIB1_df_feature
+## Output : Predicted class for each sound
+
+# Df_feature (If you have not run the previous part)
+# IIB1_df_feature <- read.table("data/features/IIB1_df_feature.txt")
+
+#libraries
+require(class)
+require(randomForest)
+
+#Functions
+get.error <- function(class,pred){
+  cont.tab <- table(class,pred)
+  print(cont.tab)
+  return((cont.tab[2,1]+cont.tab[1,2])/(sum(cont.tab)))
+}
+
+get.sensitivity <- function(class,pred){
+  cont.tab <- table(class,pred)
+  return((cont.tab[2,2])/(sum(cont.tab[2,])))
+}
+
+
+get.specificity <- function(class,pred){
+  cont.tab <- table(class,pred)
+  return((cont.tab[1,1])/(sum(cont.tab[1,])))
+}
+
+# Data train and data test
+set.seed(1234)
+n <- nrow(IIB1_df_feature)
+n.train <- round(n/4, 0)
+n.test <- n-n.train
+ind.train <- sample(1:nrow(IIB1_df_feature),n.train)
+
+x_train <- IIB1_df_feature[ind.train, 4:20]
+y_train <- as.data.frame(IIB1_df_feature[ind.train, "annotation"])
+y_train <- as.factor(y_train)
+x_test <- IIB1_df_feature[-ind.train, 4:20]
+y_test <- IIB1_df_feature[-ind.train, "annotation"]
+
+# Knn algorithm
+pred.test.knn.1 <- knn(train= x_train, test=x_test, cl= y_train, k=1)
+
+# Random Forest algorithm
+
+# Comparison Knn VS Random Forest _ ROC Curve
+
+get.error(y_test, pred.test.knn.1)
+get.specificity(y_test, pred.test.knn.1)
+get.sensitivity(y_test, pred.test.knn.1)
+
+library(ROCR)
+AUC <- matrix(NA, nrow=5, ncol=1)
+colnames(AUC) <- c("AUC") 
+rownames(AUC) <- c("KNN", "RF")
+
+knn_model <- knn(train=data.train[,4:20],test=data.test[,4:20],cl= data.train$annotation,k=10, prob = TRUE)
+prob <- attr(knn_model, "prob")
+prob <- 2*ifelse(knn_model == "-1", prob,1-prob) - 1
+pred_knn <- prediction(prob, data.test$annotation)
+performance_knn <- performance(pred_knn, "tpr", "fpr")
+#plot(performance_knn, col = 5, lwd = 1)
+plot(performance_knn, col = 5, lwd = 1, add = TRUE)
+
+pred_RF <- predict(model.50, newdata = data.test[,4:20], type = "prob")
+pred_class <-  prediction(pred_RF[,2], data.test$annotation)
+performance_RF <- performance(pred_class,measure = "tpr",x.measure= "fpr")
+plot(performance_RF, col = 3, lwd = 2, add = TRUE)
+abline(0,1)
+legend("bottomright", col = c(5,3), legend = c("Knn", "Random Forest"), fill = c(5,3))
