e960b549cd7df55961fb2301aca15e29e79817d0
AuBony <audrey.bony@yahoo.fr>
2021-02-10 11:21:33 +0100

    Creation IIB2 et update IIB1

---
+++ Cat_accoustic/script/II-B-2-Machine_Learning.R
@@ -0,0 +1,7 @@
+#######################################################################
+# PROJECT : CAT ACCOUSTIC DATA PROJECT
+# TITLE : IIB2 - Method 1
+# AUTHOR : BONY Audrey & de CROUTTE Anne-Victoire (AGROCAMPUS OUEST)
+# DATE : DECEMBER 2020 TO FEBRURARY 2021
+#######################################################################
+
